7ffad2cbc49b24993437d54d7a63cca35ab89188
Keesun Baik <whiteship2000@gmail.com>
2019-10-25 18:28:09 -0700

    4. JUnit 5: Compare to JUnit 4 annotations

--- src/test/java/me/whiteship/demospringboot22/StudyTests.java
+++ src/test/java/me/whiteship/demospringboot22/StudyTests.java
@@ -1,12 +1,31 @@
 package me.whiteship.demospringboot22;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("스터디 테스트")
 public class StudyTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
 
     @Test
     @DisplayName("스터디 만들기")
@@ -25,6 +44,7 @@ public class StudyTests {
     }
 
     @Test
+    @Disabled
     @DisplayName("스터디 만들기 에러: 최대 참석자 수가 음수인 경우")
     void createNewStudyError_as_MaxNumberOfParticipant_is_minus() {
         assertThrows(IllegalArgumentException.class, () -> Study.builder()
