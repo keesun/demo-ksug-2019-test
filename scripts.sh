2cbaf77a9db8ef3ab3066630787b2216f77aadcd
Keesun Baik <whiteship2000@gmail.com>
2019-10-31 22:42:21 -0700

    Update script

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,9 @@
 # Docker Scripts
 
-## Run PostgreSQL for
+## Run PostgreSQL
+
+### 스터디 멤버 DB
+docker run -p 15432:5432 --name study-member -e POSTGRES_USER=ksug2019 -e POSTGRES_PASSWORD=keesun -e POSTGRES_DB=member -d postgres
+
+### 스터디 키워드 DB
+docker run -p 5432:5432 --name study-keyword -e POSTGRES_USER=ksug2019 -e POSTGRES_PASSWORD=keesun -e POSTGRES_DB=study -d postgres
