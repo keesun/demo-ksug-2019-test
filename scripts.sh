28f41affe10a3ebe934ed09e157f866875baeeed
Keesun Baik <whiteship2000@gmail.com>
2019-10-31 23:03:44 -0700

    Use PostgreSQL for test

--- scripts.sh
+++ scripts.sh
@@ -2,8 +2,11 @@
 
 ## Run PostgreSQL
 
-### 스터디 멤버 DB
+### 개발용 멤버 DB
 docker run -p 15432:5432 --name study-member -e POSTGRES_USER=ksug2019 -e POSTGRES_PASSWORD=keesun -e POSTGRES_DB=member -d postgres
 
-### 스터디 키워드 DB
+### 개발용 스터디 DB
 docker run -p 5432:5432 --name study-keyword -e POSTGRES_USER=ksug2019 -e POSTGRES_PASSWORD=keesun -e POSTGRES_DB=study -d postgres
+
+### 테스트용 스터디 DB
+docker run -p 11111:5432 --name test-study-keyword -e POSTGRES_USER=ksug2019 -e POSTGRES_PASSWORD=keesun -e POSTGRES_DB=study -d postgres
